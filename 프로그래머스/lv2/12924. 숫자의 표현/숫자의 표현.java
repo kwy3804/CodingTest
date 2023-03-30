9eccab117984b972b2a44802fef737fbf0e56b29
kwy3804 <37615953+kwy3804@users.noreply.github.com>
2023-03-30 10:31:26 +0900

    [level 2] Title: 숫자의 표현, Time: 1.20 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int sum = 0;
+        
+        for(int i = 1; i <= n; i++) {
+            for(int j = i; j <= n; j++) {
+                sum += j;
+                if(sum == n)
+                    answer++;
+                else if(sum > n)
+                    break;
+            }
+            sum = 0;
+        }
+        return answer;
+    }
+}
